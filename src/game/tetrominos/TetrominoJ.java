fb4b6793fef222eeb94ab63f3718d2231d736cad
brian <brianclemente@outlook.com>
2021-10-12 16:48:00 -0300

    Solucion bug TetrominoJ se intercambio por TetrominoL

--- src/game/tetrominos/TetrominoJ.java
+++ src/game/tetrominos/TetrominoJ.java
@@ -1,50 +1,51 @@
 package game.tetrominos;
 
 import game.Grid;
+
 public class TetrominoJ extends Tetromino{
 	
 	/**
-	 * Crea un nuevo tetromino de tipo L con una grilla.
+	 * Crea un nuevo tetromino de tipo J con una grilla.
 	 */
 	public TetrominoJ(Grid grilla) {
 		super(grilla);
 		bloques[0] = miGrilla.getBloque(1, 4);
-		bloques[1] = miGrilla.getBloque(1, 3);
-		bloques[2] = miGrilla.getBloque(1, 5);
-		bloques[3] = miGrilla.getBloque(0, 5);
+		bloques[1] = miGrilla.getBloque(1, 5);
+		bloques[2] = miGrilla.getBloque(1, 3);
+		bloques[3] = miGrilla.getBloque(0, 3);
 		miRepresentacion = new TetrominoGrafico("blue");
 	}
 	
 	@Override
 	protected void correrA() {
 		this.corrimientos[0].setFyC(0, 0);
-		this.corrimientos[1].setFyC(-1, 1);
-		this.corrimientos[2].setFyC(1, -1);
-		this.corrimientos[3].setFyC(2, 0);
+		this.corrimientos[1].setFyC(1, -1);
+		this.corrimientos[2].setFyC(-1, 1);
+		this.corrimientos[3].setFyC(0, 2);
 	}
 	
 	@Override
 	protected void correrB() {
 		this.corrimientos[0].setFyC(0, 0);
-		this.corrimientos[1].setFyC(1, 1);
-		this.corrimientos[2].setFyC(-1, -1);
-		this.corrimientos[3].setFyC(0, -2);
+		this.corrimientos[1].setFyC(-1, -1);
+		this.corrimientos[2].setFyC(1, 1);
+		this.corrimientos[3].setFyC(2, 0);
 	}
 	
 	@Override
 	protected void correrC() {
 		this.corrimientos[0].setFyC(0, 0);
-		this.corrimientos[1].setFyC(1, -1);
-		this.corrimientos[2].setFyC(-1, 1);
-		this.corrimientos[3].setFyC(-2, 0);
+		this.corrimientos[1].setFyC(-1, 1);
+		this.corrimientos[2].setFyC(1, -1);
+		this.corrimientos[3].setFyC(0, -2);
 	}
 	
 	@Override
 	protected void correrD() {
 		this.corrimientos[0].setFyC(0, 0);
-		this.corrimientos[1].setFyC(-1, -1);
-		this.corrimientos[2].setFyC(1, 1);
-		this.corrimientos[3].setFyC(0, 2);
+		this.corrimientos[1].setFyC(1, 1);
+		this.corrimientos[2].setFyC(-1, -1);
+		this.corrimientos[3].setFyC(-2, 0);
 	}
 	
 }
